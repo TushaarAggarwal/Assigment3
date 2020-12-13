2080968736a5295c78f0b056101db838e6b31c17
Tushar Aggarwal <tushar.aggarwal@pragmasys.in>
2020-12-13 16:32:22 +0530

    Add project files.

---
+++ Assigment3/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Threading;
+
+namespace Assigment3
+{
+
+
+     class bank
+
+    {
+        static int netbalance = 1000;
+
+        public static void Deposite(int TransAmount)
+        {
+
+            for (int i = 0; i <= 10; i++)
+            {
+                netbalance += TransAmount;
+                Console.WriteLine(" Current Balance After Deposite=" + netbalance);
+
+                 if(i==5)
+                {
+                    Thread.Sleep(5000);
+                }
+
+            }
+
+
+
+        }
+
+        public  static void WidthDrawal(int TransAmount)
+        {
+
+            for (int i = 0; i <=10; i++)
+            {
+
+                netbalance -= TransAmount;
+                Console.WriteLine(" Current Balance After Widthrawal=" + netbalance);
+
+            }
+
+        }
+
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Amount");
+
+            int amt = Convert.ToInt32(Console.ReadLine());
+
+            bank.Deposite(amt);
+            bank.WidthDrawal(amt);
+           
+        }
+    }
+}
